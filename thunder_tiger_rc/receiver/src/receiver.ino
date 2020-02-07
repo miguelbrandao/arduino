ff04124a4d3c7e18e35ae08991f4bd3864f78326
Miguel Brandão <mickael.a.brandao@gmail.com>
2020-02-07 10:14:20 +0000

    Add full servo calibration and data transmission

--- thunder_tiger_rc/receiver/src/receiver.ino
+++ thunder_tiger_rc/receiver/src/receiver.ino
@@ -3,6 +3,12 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+typedef struct data
+{
+  int acc;
+  int dir;
+} Data;
+
 #define LED_PIN 13
 
 /************   RADIO VARIABLES   ************/
@@ -10,96 +16,127 @@
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
-
 /************   SERVO VARIABLES   ************/
-
+// DIR calibrated
 #define ACC_PIN 9
-#define ACC_MIN 800 //not calibrated
-#define ACC_MAX 2400 //not calibrated
+#define ACC_MIN 600
+#define ACC_MAX 2400
 
-// DIR calibrado
+// DIR calibrated
 #define DIR_PIN 10
 #define DIR_MIN 870
 #define DIR_MAX 2470
 
+// Range where servos can move (in degrees)
+#define ACC_MIN_POS 21
+#define ACC_MAX_POS 60
+#define DIR_MIN_POS 57
+#define DIR_MAX_POS 115
+
 Servo acc_servo;
 Servo dir_servo;
+Data data;
 
-int pos = 0;
+long acc_pos = 0;
+long dir_pos = 0;
 
+// Number of degrees of movement per servo
+long acc_range;
+long dir_range;
+
+unsigned long last_reception_millis;
+unsigned long now;
 
 /************   SETUP CODE   ************/
 
-void servo_setup(){
-  acc_servo.attach(ACC_PIN,ACC_MIN,ACC_MAX);
-  dir_servo.attach(DIR_PIN,DIR_MIN,DIR_MAX);
+void servo_start_pos()
+{
+  acc_servo.write(47);
+  dir_servo.write(89);
+}
+
+void servo_setup()
+{
+  acc_range = ACC_MAX_POS - ACC_MIN_POS;
+  dir_range = DIR_MAX_POS - DIR_MIN_POS;
+  acc_servo.attach(ACC_PIN, ACC_MIN, ACC_MAX);
+  dir_servo.attach(DIR_PIN, DIR_MIN, DIR_MAX);
   // Servos at starting position
-  acc_servo.write(0);
-  dir_servo.write(90);
+  servo_start_pos();
   delay(1000);
 }
 
-void radio_setup(){
-  Serial.begin(9600);
+void radio_setup()
+{
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
 
-
-void setup() {
-  //servo_setup();
+void setup()
+{
+  Serial.begin(115200);
+  servo_setup();
   radio_setup();
 }
 
-
 /************   ACTUAL CODE   ************/
 
-void blink(){
-
-  int blinks = 5;
-
-  for(int i = 0; i<blinks; i++){
-    delay(100);
-    digitalWrite(LED_PIN,HIGH);
-    delay(100);
-    digitalWrite(LED_PIN,LOW);
-  }
-  
-  delay(100);
-
-}
-
-void servo_loop(){
-  dir_servo.write(0);
-  delay(2000);
-  dir_servo.write(90);
-  delay(1000);
-  dir_servo.write(180);
-  delay(1000);
-  blink();
-
-//  for(pos=0; pos<=180; pos++){
-//    acc_servo.write(pos);
-//    delay(15);
-//  }
-//
-//  for(pos=180; pos>=0; pos--){
-//    acc_servo.write(pos);
-//    delay(15);
-//  }
-}
-
-void radio_loop(){
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+void failsafe()
+{
+  now = millis();
+  if (now - last_reception_millis > 2000)
+  {
+    servo_start_pos();
+    delay(2000);
   }
 }
 
-void loop() {
-  //servo_loop();
-  radio_loop();
+void radio_receive()
+{
+  if (radio.available())
+  {
+    radio.read(&data, sizeof(Data));
+    last_reception_millis = millis();
+    Serial.print("ACC: ");
+    Serial.print(abs(data.acc-1023));
+    // Serial.print(data.acc);
+    Serial.print("    DIR: ");
+    Serial.println(abs(data.dir-1023));
+    // Serial.println(data.dir);
+  }
+  else
+  {
+    failsafe();
+  }
+}
+
+void value_conversion()
+{
+  // prevent division by 0
+  long aux_acc = acc_range * abs(data.acc-1023);
+  long aux_dir = dir_range * abs(data.dir-1023);
+  if (aux_acc != 0)
+    aux_acc = aux_acc / 1023;
+  if (aux_dir != 0)
+    aux_dir = aux_dir / 1023;
+
+  acc_pos = aux_acc + ACC_MIN_POS;
+  dir_pos = aux_dir + DIR_MIN_POS;
+}
+
+void servo_update()
+{
+  acc_servo.write(acc_pos);
+  Serial.println(acc_pos);
+  dir_servo.write(dir_pos);
+  //delay(2000);
+}
+
+void loop()
+{
+  radio_receive();
+  value_conversion();
+  servo_update();
 }
--- thunder_tiger_rc/transmitter/src/main.cpp
+++ thunder_tiger_rc/transmitter/src/main.cpp
@@ -1,29 +1,62 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
+typedef struct data
+{
+  int acc;
+  int dir;
+} Data;
+
+int ledPin = 13;
+int joyPinAcc = A0;
+int joyPinDir = A1;
+Data data;
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
+Data dataBuf;
 
-void setup() {
+/*************  SETUP  *************/
+
+void radio_setup(){
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 }
 
-void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(joyPinAcc, INPUT);
+  pinMode(joyPinDir, INPUT);
+  digitalWrite(ledPin, LOW);
+  // Serial.begin(115200);
+  radio_setup();
 }
+
+
+/*************  CODE  *************/
+
+void radio_send() {
+  dataBuf = data;
+  radio.write(&dataBuf, sizeof(Data));
+}
+
+void serial_print(){
+  Serial.print("ACC: ");
+  Serial.print(data.acc);
+  Serial.print("\t\tDIR: ");
+  Serial.println(data.dir);
+  Serial.print("\n");
+}
+
+ void loop() {
+  data.acc = analogRead(joyPinAcc);  
+  // this small pause is needed between reading
+  // analog pins, otherwise we get the same value twice
+  delay(1);            
+  data.dir = analogRead(joyPinDir);  
+  //serial_print();
+  radio_send();
+ }
